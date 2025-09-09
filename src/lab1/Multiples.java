c0132d953540f32b80dc0ee401c37d11d1deb601
Zixiao Wang <shawnzixiao.wang@mail.utoronto.ca>
2025-09-08 23:03:30 -0400

    Task

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -6,7 +6,7 @@ public class Multiples {
         System.out.println(a);
     }
 
-    static int multiples(int n, int b, int a) {
+    public static int multiples(int n, int b, int a) {
         int count = 0;
         for (int i = 1; i < n; i++) {
             if (i % a == 0 ||  i % b == 0) {
