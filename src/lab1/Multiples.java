fba2b03828ec0d27b5917f6f058c31f1959bf643
Zixiao Wang <shawnzixiao.wang@mail.utoronto.ca>
2025-09-08 20:53:42 -0400

    Task4.0

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -3,11 +3,14 @@ package lab1;
 public class Multiples {
     public static void main(String[] args) {
         int i = 0;
+        int a = 0;
         while (i <= 100) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println(i);
+            if (i % 3 == 0 ||  i % 5 == 0) {
+                a++;
             }
+            i++;
         }
+        System.out.println(a);
     }
 
 }
